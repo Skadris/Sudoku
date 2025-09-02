c1dcf0779ebd095c4c0aa024eb3821640439443a
SKD <Dragos.Marian@outlook.com>
2025-09-02 18:19:15 +0300

    run black src tests

--- tests/test_board.py
+++ tests/test_board.py
@@ -3,7 +3,7 @@ import pytest
 from skdsudoku.engine.board import Board
 
 
-def test_board_init_and_get_set():   
+def test_board_init_and_get_set():
     b = Board()
     assert b.get(0, 0) == 0
 
@@ -11,9 +11,10 @@ def test_board_init_and_get_set():
     assert b.get(0, 0) == 0
     assert b2.get(0, 0) == 5
 
+
 def test_set_and_clear_multiple_cells():
-    b = Board() 
-    b2 = b.set(0, 0 , 1).set(1, 1, 2).set(2, 2, 3)
+    b = Board()
+    b2 = b.set(0, 0, 1).set(1, 1, 2).set(2, 2, 3)
     assert b2.get(0, 0) == 1
     assert b2.get(1, 1) == 2
     assert b2.get(2, 2) == 3
@@ -21,6 +22,7 @@ def test_set_and_clear_multiple_cells():
     assert b3.get(0, 0) == 1
     assert b3.get(1, 1) == 0
     assert b3.get(2, 2) == 0
+
 
 def test_board_shape_height():
     with pytest.raises(ValueError):
@@ -36,6 +38,7 @@ def test_board_value_range():
     with pytest.raises(ValueError):
         Board([[10] * 9 for _ in range(9)])
 
+
 def test_invalid_indices():
     b = Board()
     with pytest.raises(IndexError):
@@ -47,7 +50,8 @@ def test_invalid_indices():
     with pytest.raises(IndexError):
         b.clear(0, -1)
 
-def test_box_values_and_empty_cells():   
+
+def test_box_values_and_empty_cells():
     b = Board()
     b = b.set(1, 1, 4)
     assert 4 in b.box_values(2, 2)
@@ -56,12 +60,14 @@ def test_box_values_and_empty_cells():
     assert (1, 1) not in em
     assert len(em) == 80
 
+
 def test_rown_col_box_access():
     b = Board()
     b = b.set(0, 0, 5).set(0, 1, 3).set(1, 0, 6)
-    assert b.row_values(0) == [5, 3] + [0]*7
-    assert b.col_values(0) == [5, 6] + [0]*7
-    assert set(b.box_values(0, 0)) == {5,3,6,0}
+    assert b.row_values(0) == [5, 3] + [0] * 7
+    assert b.col_values(0) == [5, 6] + [0] * 7
+    assert set(b.box_values(0, 0)) == {5, 3, 6, 0}
+
 
 def test_clear_cell():
     b = Board()
@@ -70,6 +76,7 @@ def test_clear_cell():
 
     b = b.clear(2, 3)
     assert 5 not in b.box_values(2, 3)
+
 
 def test_empty_cells_partial():
     b = Board()
@@ -80,6 +87,7 @@ def test_empty_cells_partial():
     assert (1, 1) not in em
     assert (0, 1) in em
     assert (8, 8) in em
+
 
 def test_immutability():
     b = Board()
